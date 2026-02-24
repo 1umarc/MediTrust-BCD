f00eb6e5daa76cd76dee5ebf88bf26a387e563fa
ddragonffruit <xxiaowwen@gmail.com>
2026-02-24 09:55:42 +0800

    Frontend_Images

--- frontend/src/app/images/index.ts
+++ frontend/src/app/images/index.ts
@@ -11,6 +11,33 @@ import CommunityDonate from '@/app/images/CommunityDonate.png'
 import Email from '@/app/images/Email.png'
 import GroupUsers from '@/app/images/GroupUsers.png'
 import KeyUnlock from '@/app/images/KeyUnlock.png'
+import Pending from '@/app/images/Pending.png'
+import Approve from '@/app/images/Approve.png'
+import Reject from '@/app/images/Reject.png'    
+import EmptyState from '@/app/images/EmptyState.png'
+import HospitalPanel from '@/app/images/HospitalPanel.png'
+import ExclamationMark from '@/app/images/ExclamationMark.png'
+import CorrectGreen from '@/app/images/CorrectGreen.png'
+import RejectRed from '@/app/images/RejectRed.png'
+import Caution from '@/app/images/Caution.png'
+import AdminPanel from '@/app/images/AdminPanel.png'
+import Admin_HospitalReps from '@/app/images/Admin_HospitalReps.png'
+import Admin_TotalAdmin from '@/app/images/Admin_TotalAdmin.png'
+import AdminGroupUsers from '@/app/images/AdminGroupUsers.png'
+import Admin_AddUsers from '@/app/images/Admin_AddUsers.png'
+import Admin_RemoveUsers from '@/app/images/Admin_RemoveUsers.png'
+import Admin_AddHospitalReps from '@/app/images/Admin_AddHospitalReps.png'
+import Admin_Caution from '@/app/images/Admin_Caution.png'
+import CorrectMark from '@/app/images/CorrectMark.png'
+import DAOVoting from '@/app/images/DAOVoting.png'
+import MoneyIcon from '@/app/images/MoneyIcon.png'
+import FireIcon from '@/app/images/FireIcon.png'
+import AddIcon from '@/app/images/AddIcon.png'
+import CampaignEmptyState from '@/app/images/CampaignEmptyState.png'
+import User from '@/app/images/User.png'
+import ProcessIcon from '@/app/images/ProcessIcon.png'
+import OnGoingIcon from '@/app/images/OnGoingIcon.png'
+import TotalCampaignIcon from '@/app/images/TotalCampaignIcon.png'
 
 export {
     MediTrustLogo,
@@ -25,5 +52,32 @@ export {
     CommunityDonate,
     Email,
     GroupUsers,
-    KeyUnlock
+    KeyUnlock,
+    Pending,
+    Approve,
+    Reject,
+    EmptyState,
+    HospitalPanel,
+    ExclamationMark,
+    CorrectGreen,
+    RejectRed,
+    Caution,
+    AdminPanel,
+    Admin_HospitalReps,
+    Admin_TotalAdmin,
+    AdminGroupUsers,
+    Admin_AddUsers,
+    Admin_RemoveUsers,
+    Admin_AddHospitalReps,
+    Admin_Caution,
+    CorrectMark,
+    DAOVoting,
+    MoneyIcon,
+    FireIcon,
+    AddIcon,
+    CampaignEmptyState,
+    User,
+    ProcessIcon,
+    OnGoingIcon,
+    TotalCampaignIcon
 }
